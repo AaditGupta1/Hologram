b684f28a3f836a79c4968b16d9ba3c1e51d45143
AaditGupta1 <86959222+AaditGupta1@users.noreply.github.com>
2024-03-10 16:13:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+link = prompt()
+
+document.querySelector('.a').src = link;
+document.querySelector('.b').src = link;
+document.querySelector('.c').src = link;
+document.querySelector('.d').src = link;
